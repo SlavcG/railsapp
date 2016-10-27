a1d3872d3dc7bdf6b1f221e4819bf45772cb232a
SlavcG <slavkogalich@gmail.com>
2016-10-27 16:59:22 +0200

    4.5 fix

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.serve_static_assets = true
-config.assets.compile = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,5 +12,7 @@ module Railsapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
+    config.assets.compile = true
   end
 end
