018ba31c662bae646bf14f715a5adbac7bc4902d
SlavcG <slavkogalich@gmail.com>
2017-02-03 09:48:38 +0100

    6.15 payment fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,6 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
-  }
-else
-  Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_hovSLdH9ukwP0pKM2IGrtkcm',
-    :secret_key => 'sk_test_Qe3eB7px5U5GfDdkaJwtesyf'
-  }
-end
+Rails.configuration.stripe = {
+  :publishable_key => 'pk_test_hovSLdH9ukwP0pKM2IGrtkcm',
+  :secret_key => 'sk_test_Qe3eB7px5U5GfDdkaJwtesyf'
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
